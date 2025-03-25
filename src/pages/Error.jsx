5c1f5e12cc2a8e8c284eea7df69f37308b977035
tihomirtx88 <tihomirtx88@gmail.com>
2025-03-25 10:00:20 +0200

    Implement error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,28 @@
+import { useRouteError, Link} from 'react-router-dom';
+
 const Error = () => {
+    const error = useRouteError();
+    console.log(error);
+
+    if (error.status === 404) {
+        return(
+            <main className='grid min-h-[100vh] place-items-center px-8'>
+              <div className='text-center'>
+                <p className='text-9xl font-semibold text-primary'>404</p>
+                <h1 className='mt-4 text-3xl font-bold tracking-tight sm:text=5xl'>page not found</h1>
+                <p className='mt-6 text-lg leading-7'>Sorry, we can find the page you are loking for.</p>
+                <div className='mt-10'>
+                    <Link to='/' className='btn btn-secondary'>go back home</Link>
+                </div>
+              </div>
+            </main>
+        );
+    };
+    
     return(
-        <div>Error</div>
+        <main className='grid min-h-[100vh] place-items-center px-8'>
+          <h4 className='text-center font-bold text=4xl'>there was the errror</h4>
+        </main>
     );
 };
 
