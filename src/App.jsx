fa318d447947e344af95c2e1b12027979b1cb42f
tihomirtx88 <tihomirtx88@gmail.com>
2025-03-25 09:17:41 +0200

    Implement router structore

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
-import { About, Cart, Checkout, Error, HomeLayout, Landing, Orders, Products, Register, SingleProduct } from './pages';
+import { About, Cart, Checkout, Login, Error, HomeLayout, Landing, Orders, Products, Register, SingleProduct } from './pages';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        index: true,
+        element: <Landing/>
+      },
+      {
+        path: 'products',
+        element: <Products/>
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct/>
+      },
+      {
+        path: 'cart',
+        element: <Cart/>
+      },
+      {
+        path: 'orders',
+        element: <Orders/>
+      },
+      {
+        path: 'about',
+        element: <About/>
+      },
+      {
+        path: 'checkout',
+        element: <Checkout/>
+      },
+    ]
+  },
+  {
+    path: '/login',
+    element: <Login/>,
+    errorElement: <Error/>
+  },
+  {
+    path: '/register',
+    element: <Register/>,
+    errorElement: <Error/>
+  },
+]);
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+   <RouterProvider router={router}/>
   )
 }
 
--- src/pages/idnex.js
+++ src/pages/idnex.js
@@ -8,3 +8,4 @@ export {default as Orders} from './Orders';
 export {default as Products} from './Products';
 export {default as Register} from './Register';
 export {default as SingleProduct} from './SingleProduct';
+export {default as Login} from './Login';
