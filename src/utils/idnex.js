c4f171642f962fdf95f8c7ec648fa257992b59d5
tihomirtx88 <tihomirtx88@gmail.com>
2025-03-27 15:56:00 +0200

    Fetch featured products

--- src/utils/idnex.js
+++ src/utils/idnex.js
@@ -6,3 +6,11 @@ export const customFetch = axios.create({
     baseURL: productionUrl,
     
 });
+
+export const formatPrice = (price) => {
+    const dollarsAmount = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format((price/100).toFixed(2));
+    return dollarsAmount;
+};
