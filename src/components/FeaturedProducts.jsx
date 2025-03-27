dbeb1229368cec2cee6709a34e524618a3c8b2b4
tihomirtx88 <tihomirtx88@gmail.com>
2025-03-27 20:04:25 +0200

    Fetching single product

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -1,6 +1,7 @@
 import ProductsGrid from './ProductsGrid';
 import SectionTitle from './SectionTitle';
 
+
 const FeaturedProducts = () => {
   return (
     <div className='pt-24 max-w-[95%] m-auto'>
