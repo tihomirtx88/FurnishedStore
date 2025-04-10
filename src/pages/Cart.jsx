b8389de5b51189afc911b5bb77787cf3d83dd16b
tihomirtx88 <tihomirtx88@gmail.com>
2025-04-10 09:25:38 +0300

    Implementing cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,41 @@
+import { useSelector } from "react-redux";
+import { CartItemsList, CartTotals, SectionTitle } from "../components";
+import { Link } from "react-router-dom";
+
 const Cart = () => {
-    return(
-        <div>Cart</div>
-    );
+  //Temp
+  const user = null;
+  const numItemsInCart = useSelector(
+    (state) => state.cartState.numItemsInCart
+  );
+  console.log(numItemsInCart);
+  
+
+  if (!numItemsInCart) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+  return (
+    <>
+      <SectionTitle text="Your cart is empty" />
+      <div className="mt-8 grid gap-8 lg:grid-cols-12">
+        <div className="lg:col-span-8">
+          <CartItemsList />
+        </div>
+        <div className="lg:col-span-4 lg:pl-4">
+          <CartTotals />
+          {user ? (
+            <Link to="/checkout" className="btn btn-primary btn-block mt-8">
+              procced to checkout
+            </Link>
+          ) : (
+            <Link to="/login" className="btn btn-primary btn-block mt-8">
+              please login
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Cart;
