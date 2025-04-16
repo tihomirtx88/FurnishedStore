8b9ec320e48a0081e9ccb156f4763eda90c1a14f
tihomirtx88 <tihomirtx88@gmail.com>
2025-04-16 09:12:04 +0300

    Setup login functionality in login page and use action in app.js

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,36 @@
+import { toast } from 'react-toastify';
+import { customFetch } from '../utils/idnex';
 import { SubmitBtn, FormInput} from './../components';
-import { Link, Form } from 'react-router-dom';
+import { Link, Form, redirect } from 'react-router-dom';
+import { loginUser } from '../features/user/userSlice';
+
+export const action = ( store ) => async ({request}) => {
+  // Action recive function (store)
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    const response = await customFetch.post('/auth/login', data);
+    // Pass response data to userslice
+    store.dispatch(loginUser(response.data));
+    console.log(response);
+    
+    toast.success('Account login successfully');
+    redirect('/');
+  } catch (error) {
+    const errorMessage = error?.response?.data?.error?.message || 'Please double check your credentials!';
+    toast.error(errorMessage);
+    return null;
+  }
+};
 
 const Login = () => {
     return(
         <section className='h-screen grid place-items-center'>
           <Form method='POST' className='card w-96 py-8 px-8 bg-base-100 shadow-lg flex flex-col gap-y-4'>
             <h4 className='text-center text-3xl font-bold'>Login</h4>
-            <FormInput type='email' label='email' name='identifier' defaultValue='test@abv.bg'/>
-            <FormInput type='password' label='passowrd' name='password' defaultValue='secret'/>
+            <FormInput type='email' label='email' name='email'/>
+            <FormInput type='password' label='passowrd' name='password'/>
             <div className='mt-4'>
                 <SubmitBtn type='login'/>
             </div>
