d109cdab6ac61f33a2e02cfcfd4cc4da2763f4c1
tihomirtx88 <tihomirtx88@gmail.com>
2025-04-21 15:54:04 +0300

    Implement react query qlient for all products page

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}/>
-      <ReactQueryDevtools initialIsOpen={true}/>
+      <ReactQueryDevtools initialIsOpen={false}/>
     </QueryClientProvider>
   )
 }
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,18 @@
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Loading from "../components/Loading";
+import { useIsFetching } from "@tanstack/react-query";
 
 const HomeLayout = () => {
-  const navigation = useNavigation();
-  const isPageLoading = navigation.state === 'loading';
+  // const isFetching = useIsFetching();
   return (
     <>
       <div className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-white transition-all duration-300">
         <Header />
         <Navbar />
-        {isPageLoading ? <Loading/> :  <Outlet />}
+        {/* {isFetching ? <Loading /> : <Outlet />} */}
+        <Outlet />
       </div>
     </>
   );
