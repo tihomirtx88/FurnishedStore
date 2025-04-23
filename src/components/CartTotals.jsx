6b6b82796b83d629f21a6f3a5fce417071359b9d
tihomirtx88 <tihomirtx88@gmail.com>
2025-04-23 09:16:01 +0300

    Changes on styling

--- src/components/CartTotals.jsx
+++ src/components/CartTotals.jsx
@@ -6,28 +6,28 @@ const CartTotals = () => {
     (state) => state.cartState
   );
   return (
-    <div className="max-w-md mx-auto mt-8 p-6 border border-base-300 rounded-xl shadow-md bg-base-800">
+    <div className="min-w-64 w-auto mx-auto mt-8 p-6 border border-base-300 rounded-xl shadow-md bg-white dark:bg-gray-800">
       <div className="space-y-3">
         {/* Subtotal */}
-        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-white">
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-gray-800 dark:text-white">
           <span>Subtotal</span>
           <span className="font-medium">{formatPrice(cartTotal)}</span>
         </p>
 
         {/* Shipping */}
-        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-white">
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-gray-800 dark:text-white">
           <span>Shipping</span>
           <span className="font-medium">{formatPrice(shipping)}</span>
         </p>
 
         {/* Tax */}
-        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-white">
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-gray-800 dark:text-white">
           <span>Tax</span>
           <span className="font-medium">{formatPrice(tax)}</span>
         </p>
 
         {/* Order Total */}
-        <p className="flex justify-between text-base font-semibold text-white">
+        <p className="flex justify-between text-sm border-b border-base-300 pb-2 text-gray-800 dark:text-white">
           <span>Total</span>
           <span>{formatPrice(orderTotal)}</span>
         </p>
