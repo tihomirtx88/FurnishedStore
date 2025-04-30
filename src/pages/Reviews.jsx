100ca018b1ccbe641051f99eab6d0dfe90587a6e
tihomirtx88 <tihomirtx88@gmail.com>
2025-04-30 12:00:53 +0300

    Implement functionality for fetch reviews in reviews page

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -1,15 +1,59 @@
-import { useLoaderData } from "react-router-dom";
+import { redirect } from "react-router-dom";
+import { customFetch } from "../utils/idnex";
+import { toast } from "react-toastify";
+import { PaginationContainer, ReviewsList } from "../components";
+
+const reviewsQuery = (params, user) => {
+  return {
+    queryKey: [
+      "orders",
+      user.username,
+      params.page ? parseInt(params.page) : 1,
+    ],
+    queryFn: () =>
+      customFetch("/reviews", {
+        params,
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      }),
+  };
+};
+
+export const loader = ( queryClient, store ) => async ({request}) => {
+    const user = store.getState().userState.user;
+
+    if (!user) {
+        toast.warn("You must to be logged in to veiw orders");
+        redirect("/login");
+    }
+
+    const params = Object.fromEntries([
+        ...new URL(request.url).searchParams.entries(),
+    ]);
+
+    try {
+        const response = await queryClient.ensureQueryData(reviewsQuery(params, user));
+    
+        const { reviews, numOfPages, currentPage, totalProducts, count } = response.data;
+        return { reviews, numOfPages, currentPage, totalProducts, count };
+    
+    } catch (error) {
+        const msg =
+        error?.response?.data?.error?.message || "Reviews failed. Try again.";
+        toast.error(msg);
+        if (error?.response?.status === 401) return redirect("/login");
+        return null;
+    }
+};
 
 const Reviews = () => {
-
-    const { orders } = useLoaderData();
-    if (orders.length < 1) {
-      return <SectionTitle text="please make a order" />;
-    }
-    return (
-      <>
-        <SectionTitle text="Your Orders" />
-      </>
-    );
-  };
-  export default Reviews;
+  return (
+    <>
+      <SectionTitle text="All Orders" />
+      <ReviewsList/>
+      <PaginationContainer />
+    </>
+  );
+};
+export default Reviews;
